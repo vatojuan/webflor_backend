d392b1aa952f976852e39892015ef78c087801c7
vatojuan <juanchi190_16@hotmail.com>
2025-04-26 16:15:53 -0300

    propuestas1234567890-12

--- app/database.py
+++ app/database.py
@@ -3,9 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Configura la URL de conexión a PostgreSQL (ajusta usuario, contraseña, host y nombre de BD)
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Juanchi190@localhost/webflor_db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Juanchi190@localhost/webflor_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Pachamama190@db.apnfioxjddccokgkljvd.supabase.co:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
